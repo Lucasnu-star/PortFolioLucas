dd6d1946acf368c8c2e8232dc34468b35d32941c
Lucas Gabriel Nuñez <65827279+Lucasnu-star@users.noreply.github.com>
2024-01-03 23:23:48 -0300

    home

--- MePortFolio/src/Pages/Home/Home.jsx
+++ MePortFolio/src/Pages/Home/Home.jsx
@@ -1,25 +1,33 @@
-import jsonGif from './public/business-ideas.json'
+import '../../App.css';
 
 function Home() {
   return (
-    <article className="h-screen w-full flex mx-auto bg-gray-900 text-white ">
-        <section>
-            <div>
-            <h1>Lucas Nuñez</h1>
-            <h2>Desarrolador Front End</h2>
-            </div>
-            <div>
-                <button><a href="">linke</a></button>
-                <button><a href="">git</a></button>
-                <button><a href="">cv</a></button>
-            </div>
-            
-        </section>
-        <section>
-            <img src={jsonGif} alt="" />
-        </section>
+    <body className="z-10 py-4 clip-custom min-h-screen w-full flex flex-col justify-around items-center mx-auto bg-gray-900 text-white p-4 md:flex-row md:items-center">
 
-    </article>
-  )
+      <section className="mb-4 md:mb-0 md:mr-8 text-center">
+        <div>
+          <h1 className="text-4xl md:text-6xl font-bold">Lucas Nuñez</h1>
+          <h2 className="py-3 text-xl md:text-3xl font-semibold opacity-15">Desarrollador Front End</h2>
+        </div>
+        <div className=" flex flex-col md:flex-row items-center py-4 text-lg md:text-3xl">
+          <button className="mb-2 md:mb-0 text-white">
+            <a className="" href="#">
+              <img className='w-16 md:w-20 lg:w-24' src="/github-logo.svg" alt="GitHub Logo" />
+            </a>
+          </button>
+          <button className="mb-2 md:mb-0 mx-4">
+  <a className="text-white" href="">
+    <img className='w-16 md:w-20 lg:w-24 filter invert-100' src="/linkedin-logo.svg" alt="LinkedIn Logo" />
+  </a>
+</button>
+          <button className="mb-2 md:mb-0 mx-2 md:mx-4 lg:mx-5  py-2 px-4 md:px-5">
+            <a href="">CV</a>
+          </button>
+        </div>
+      </section>
+
+    </body>
+  );
 }
-export default Home
+
+export default Home;
--- MePortFolio/src/Pages/NavBar/NavBar.jsx
+++ MePortFolio/src/Pages/NavBar/NavBar.jsx
@@ -1,23 +1,18 @@
 
 
 const Navbar = () => {
- 
-
   return (
-    <nav  className="p-3  bg-gray-900 text-white border-b border-white "  >
-      <div className="container mx-auto flex justify-around  ">
-        <ul className=" space-x-6 flex ">
-          <li className=" py-3 "><a href="">Home</a></li>
-          <li className=" py-3"><a href="">About</a></li>
-          <li className=" py-3"><a href="">Services</a></li>
-          <li className="py-3 "><a href="">Contact</a></li>
+    <nav className=" z-10 fixed top-0 left-0 right-0 p-3 bg-gray-900 text-white border-b border-white">
+      <div className="container mx-auto flex justify-around">
+        <ul className="space-x-6 flex">
+          <li className="py-3"><a href="">Home</a></li>
+          <li className="py-3"><a href="">About</a></li>
+          <li className="py-3"><a href="">Services</a></li>
+          <li className="py-3"><a href="">Contact</a></li>
         </ul>
       </div>
     </nav>
   );
-
-  };
-
- 
+};
 
 export default Navbar;
